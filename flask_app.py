971d505fde644c761a35f1d045d343f890ef495a
robson <kassapete@hotmail.com>
2020-10-15 23:22:26 -0300

    sqlalchemy configurado

--- flask_app.py
+++ flask_app.py
@@ -3,27 +3,34 @@ import psycopg2
 from datetime import datetime
 from pytz import timezone
 
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 
 # configurar o banco de dados
 
-class Aluno():
-    def __init__(self,ra, nome, email, logradouro, numero,complemento):
-        self.ra = ra
-        self.nome = nome
-        self.email = email
-        self.logradouro = logradouro
-        self.numero = numero
-        self.complemento = complemento
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Impacta.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Cadastro(db.Model):
+    ra = db.Column(db.Integer, primary_key=True, nullable=True)
+    nome = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(50), nullable=True)
+    logradouro = db.Column(db.String(50), nullable=True)
+    numero = db.Column(db.String(5), nullable=True)
+    cep = db.Column(db.String(10), nullable=True)
+    complemento = db.Column(db.String(20))
 
 sql_create = '''CREATE TABLE Hair2you (RA integer 
-                PRIMARY KEY NOT NULL, nome varchar(50) NOT NULL, email_do_aluno 
+                PRIMARY KEY NOT NULL, nome varchar(50) NOT NULL, email 
                 varchar(50) NOT NULL, Logradouro varchar(50) NOT NULL,
                 numero varchar(5) NOT NULL, cep varchar(10) NOT NULL,
                 complemento varchar(20)
                 )'''
-
-
+db.create_all()
+ 
 '''def create_table():
     connection = psycopg2.connect(
        
@@ -49,28 +56,35 @@ def inicio():
     return render_template('index.html')
 
 
-
-@app.route('/aluno', methods=['GET', 'POST'])
-def aluno():
-    if request.method == "POST":
-        ra = request.form['ra']
-        nome = request.form['nome']
-        email = request.form['email_do_aluno'] 
-        logradouro = request.form['endereço'] 
-        numero = request.form['numero']
-        complemento = request.form['complemento']
-        return render_template('contato.html')
-    if ra and nome and email and logradouro and numero and complemento:
-        a = Aluno(ra, nome, email, logradouro, numero,complemento)
-        sql_create.session.add(a)
-        sql_create.commit()
-    return render_template('obrigado.html') 
-
-
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
 
+
+
+@app.route('/login1', methods=['GET','POST'])
+def login1():
+    return render_template('obrigado.html')
+
+
+
+@app.route('/aluno', methods=['GET', 'POST'])
+def aluno():
+    if request.method == "POST":
+        aluno = Cadastro(ra = request.form['ra'],
+                        nome = request.form['nome'],
+                        email = request.form['email'], 
+                        logradouro = request.form['endereco'], 
+                        numero = request.form['numero'],
+                        cep = request.form['cep'],
+                        complemento = request.form['complemento'])
+        
+        db.session.add(aluno)
+        db.session.commit()
+        return render_template('obrigado.html') 
+    
+        
+    
 
 @app.route('/obrigado')
 def obrigado():
